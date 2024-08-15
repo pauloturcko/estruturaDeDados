4a6a6bf93f5f5b6ada0cc8bc545681f550c4c69a
Paulo Cezar Turco <415525@unoesc.lan>
2024-08-14 22:07:58 -0300

    Aula 01

---
+++ Atividades/aula01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Declarando Variável
+int number;
+// Atribuindo Valor
+number = 10;
+
+// Declarando Consatnte
+const int DAYS_IN_WEEK = 7;
+
+// Impirindo String Concatenada
+Console.WriteLine( $"A semana tem {DAYS_IN_WEEK} dias.");
