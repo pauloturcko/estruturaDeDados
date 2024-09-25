aeb754115796e57d93d0b0355f20d92c0d6bd023
Paulo Cezar Turco <415525@unoesc.lan>
2024-09-25 20:58:35 -0300

    Tabela Hash e Dictionary

---
+++ Atividades/Tabela Hash/Program.cs
@@ -0,0 +1,42 @@
+﻿// Exemplo de uma agenda telefônica
+
+using System.Collections;
+
+Hashtable phoneBook = new Hashtable()
+{
+    { "Chave", "Valor" },
+    { "Thomazzi", "49-99961-0150" },
+    { "Mauicio Gonzatto", "49-99975-8575" },
+    { "Gabriel Bianchi", "49-99105-8904"}
+};
+
+// É possívl adicionar elementos de diversas formas. Pelo próprio índice chave inexistente
+phoneBook["Thiago Padilha"] = "49-99176-8255";
+
+// Ou pelo método Add()
+phoneBook.Add("Marcos Henrique", "49-99202-6169");
+
+// Entretanto, a TabelaHash verifica se há duplicidade de chave e pode lançar uma Exception
+try
+{
+    phoneBook.Add("Mauricio Gonzatto", "49-99975-8575");
+}catch(Exception ex)
+{
+    Console.WriteLine($"Erro ao tentar incluir valor repetido.");
+    Console.WriteLine(ex.Message);
+    throw ex;
+}
+
+// Percorrendo itens da HashTable
+Console.WriteLine("Agenda telefônica:");
+if(phoneBook.Count == 0)
+{
+    Console.WriteLine("A agenda está vazia.");
+}
+else
+{
+    foreach(DictionaryEntry entry in phoneBook)
+    {
+        Console.WriteLine($"{entry.Key}:{entry.Value}");
+    }
+}
---
+++ Atividades/dicionario/Program.cs
@@ -0,0 +1,40 @@
+﻿// Diferente da TabelaHash, o Dictionary nos permite expecificar o tipo de dado que será utilizado nos pares Key -> Value
+// Isso nos fornece uma estrutura fortemente tipada
+
+Dictionary<string, string> dictionary = new Dictionary<string, string>()
+{
+    { "Chave 1", "Valor 1" },
+    { "Chave 2", "Valor 2" },
+};
+
+// Obter valor do dictionary
+string val = dictionary["Chave 1"];
+
+try
+{
+    val = dictionary["Chave 3"];
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro ao obter valor da Chave 3");
+    Console.WriteLine(ex.Message);
+}
+
+// Verificando se a chave existe antes de acessar para evitar erros e prejuízos de execução
+if ( dictionary.ContainsKey("Chave 2") )
+{
+    Console.WriteLine($"Chave 2: {dictionary["Chave 2"]}");
+}
+
+// Outra forma de obter o valor e evitar erro
+dictionary.TryGetValue("Chave 4", out string? valor);
+if(valor is not null) Console.WriteLine($"Chave 4: {valor}");
+
+// Podemos adicionar da mesma forma pela chave inexistente
+dictionary["Chave 0"] = "Valor 0";
+
+// Percorrer o dicionário
+foreach(KeyValuePair<string, string> kvp in dictionary)
+{
+    Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+};
