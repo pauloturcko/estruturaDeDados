513299a68e4afa4c32fbeff4d3ec2f4f5a7ac4a1
Paulo Cezar Turco <415525@unoesc.lan>
2024-10-09 21:29:09 -0300

    Algarismos de Ordenação - BubbleSort

---
+++ Atividades/algoritimosDeOrdenacao/BubbleSort/Program.cs
@@ -0,0 +1,13 @@
+﻿using BubbleSort;
+
+int[] inteiros = { 0, 12, 7, 1, 2, 8 };
+
+BubbleSort.BubbleSort.Print(inteiros);
+BubbleSort.BubbleSort.Sort<int>(inteiros);
+BubbleSort.BubbleSort.Print(inteiros);
+
+Console.WriteLine("Teste Char");
+char[] chars = ['x', 'z', 'b', 'c', 'a'];
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+BubbleSort.BubbleSort.Print(chars);
