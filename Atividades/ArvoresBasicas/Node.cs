6c79a8bc8c25854d6721b952c625943b8ae1800a
Paulo Cezar Turco <415525@unoesc.lan>
2024-10-23 21:42:16 -0300

    Árvores

---
+++ Atividades/ArvoresBasicas/Node.cs
@@ -0,0 +1,18 @@
+namespace ArvoresBasicas
+{
+    public class Node<T>
+    {
+        public T? Data { get; set; }
+        public Node<T>? Parent { get; set; }
+        public List<Node<T>>? Children { get; set; } = [];
+        public int GetHeight() {
+            int height = 1;
+            Node<T> current = this;
+            while(current.Parent != null) {
+                height++;
+                current = current.Parent;
+            }
+            return height;
+        }
+    }
+}
---
+++ Atividades/ArvoresBasicas/Program.cs
@@ -0,0 +1,21 @@
+﻿using ArvoresBasicas;
+
+Tree<int> tree = new Tree<int>();
+tree.root = new Node<int>() { Data = 100 };
+// tree.root.Data = 100;
+
+tree.root.Children =
+    new List<Node<int>>{
+        new Node<int>() { Data = 50, Parent = tree.root },
+        new Node<int>() { Data = 1, Parent = tree.root},
+        new Node<int>() { Data = 150, Parent = tree.root},
+    };
+
+Node<int> node12 = new Node<int>() { Data = 12, Parent= tree.root.Children[0]};
+Node<int> node45 = new Node<int>() { Data = 45, Parent = node12 };
+Node<int> node21 = new Node<int>() { Data = 21, Parent = node12 };
+node12.Children!.Add(node45!);
+node12.Children!.Add(node21!);
+tree.root.Children[0].Children!.Add(node12);
+
+tree.PrintTree(tree.root);
