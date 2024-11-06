f6b0a345f4ef7861411027072fad583fe91416c7
Paulo Cezar Turco <415525@unoesc.lan>
2024-11-06 20:48:49 -0300

    Arvores Binarias

---
+++ Atividades/ArvoreBinaria/BinaryTreeNode.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// ESTUDAR SOBRE HERANÇA DO PARADIGMA DE POO
+
+namespace ArvoreBinaria
+{
+    public class BinaryTreeNode<T> : TreeNode<T>
+    {
+        public BinaryTreeNode()
+        {
+            base.Children = new List<TreeNode<T>>();
+        }
+
+        public BinaryTreeNode<T> Left
+        {
+            get { return (BinaryTreeNode<T>)Children[0]; }
+            set { Children[0] = value; }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get { return (BinaryTreeNode<T>)Children[1]; }
+            set { Children[1] = value; }
+        }
+    }
+}
